ad3d9d3b16440e17993d3e2e1dcdd32bc3f0bfb9
Alec Hampton <alhampton9@gmail.com>
2024-10-24 16:08:56 -0600

    progress

--- sorter/cmd/sorter.go
+++ sorter/cmd/sorter.go
@@ -18,54 +18,56 @@ func main() {
 			panic(err)
 		}
 	}()
-
 	inputList := make([]string, 0)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		inputList = append(inputList, scanner.Text())
 	}
-
-	// for _, item := range inputList {
-	// 	fmt.Println(item)
-	// }
 	sortInput(&inputList)
 }
 
 func sortInput(inputList *[]string) {
+	winners := make([]string, 0, len(*inputList)/2 + 1)
+	losers := make([]string, 0, len(*inputList)/2 + 1)
+
 	s1 := removeRandomElement(inputList)
 	s2 := removeRandomElement(inputList)
-	err := promptToSortValues(s1, s2)
+	higher, lower, err := promptToSortTwoInputs(s1, s2)
 	for err != nil {
 		fmt.Println("Error: Please enter either '1' or '2'")
-		err = promptToSortValues(s1, s2)
+		higher, lower, err = promptToSortTwoInputs(s1, s2)
 	}
+	winners = append(winners, higher)
+	losers = append(losers, lower)
+
+	fmt.Println("Winners: ")
+	printSlice(winners)
+	fmt.Println("Losers: ")
+	printSlice(losers)
 }
 
-func promptToSortValues( s1 string, s2 string) error {
+// promptToSortTwoInputs prompts the user to enter the higher of the inputs, then returns them in order
+func promptToSortTwoInputs( s1 string, s2 string) (string, string, error) {
 	reader := bufio.NewReader(os.Stdin)
-	// for {
-		// topHalf := make([]string, 0)
-		// bottomHalf := make([]string, 0)
-		fmt.Println("Choose the larger of: ")
-		fmt.Println("1. ", s1)
-		fmt.Println("2. ", s2)
-		fmt.Print("Enter '1' or '2' -> ")
-		input, _, err := reader.ReadRune()
+	fmt.Println("Choose the larger of: ")
+	fmt.Println("1. ", s1)
+	fmt.Println("2. ", s2)
+	fmt.Print("Enter '1' or '2' -> ")
+	input, _, err := reader.ReadRune()
 
-		fmt.Println()
-		if err != nil {
-			panic(err)
-		}
-		if input == '1' {
-			fmt.Println("good choice")
-			return nil
-		} else if input == '2' {
-			fmt.Println("good choice")
-			return nil
-		} else {
-			return errors.New("invalid input")
-		}
-	// }
+	fmt.Println()
+	if err != nil {
+		panic(err)
+	}
+	if input == '1' {
+		fmt.Println("good choice")
+		return s1, s2, nil
+	} else if input == '2' {
+		fmt.Println("good choice")
+		return s2, s1, nil
+	} else {
+		return "", "", errors.New("invalid input")
+	}
 }
 
 func removeRandomElement(inputList *[]string) string {
@@ -79,8 +81,7 @@ func removeElementAtIndex(index int, inputList *[]string) string {
 }
 
 func printSlice(inputList []string) {
-	fmt.Println()
-	fmt.Println("Printing input list: ")
+	// fmt.Println("Printing slice: ")
 	for _, item := range inputList {
 		fmt.Println(item)
 	}
