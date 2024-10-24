650c893367f9ef041a7206ba5c92e1b7f6272755
Alec Hampton <alhampton9@gmail.com>
2024-10-24 14:42:49 -0600

    Basic list read-in

--- sorter/cmd/sorter.go
+++ sorter/cmd/sorter.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hi")
+	file, err := os.Open("todo.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	inputList := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputList = append(inputList, scanner.Text())
+	}
+
+	for _, item := range inputList {
+		fmt.Println(item)
+	}
 }
